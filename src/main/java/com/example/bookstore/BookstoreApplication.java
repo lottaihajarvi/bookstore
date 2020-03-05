2ad92199eb31b99055d33eb2a6e0602ccc246629
Lotta Ihajärvi <lotta.ihajarvi@gmail.com>
2020-03-05 14:43:36 +0200

    second commit

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Bean;
 
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
+import com.example.bookstore.domain.Category;
+import com.example.bookstore.domain.CategoryRepository;
+
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -19,16 +22,20 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookstore(BookRepository repository) {
+	public CommandLineRunner bookstore(BookRepository brepository, CategoryRepository crepository) {
 		return (args) -> {
 			log.info("save a couple of books");
-			repository.save(new Book("The Hobbit", "J.R.R Tolkien", 1937, 9783168, 29.95));
-			repository.save(new Book("Pet Semetary", "Stephen King", 1983, 4567165, 15.95));
+			crepository.save(new Category("Fantasy"));
+			crepository.save(new Category("Horror"));
+			crepository.save(new Category("Scifi"));
+			
+			brepository.save(new Book("The Hobbit", "J.R.R Tolkien", 1937, 9783168, 29.95, crepository.findByName("Fantasy").get(0)));
+			brepository.save(new Book("Pet Semetary", "Stephen King", 1983, 4567165, 15.95, crepository.findByName("Horror").get(0)));
 			
 			log.info("fetch all books");
-			for (Book book : repository.findAll()) {
+			for (Book book : brepository.findAll()) {
 				log.info(book.toString());
 			}
-			};
-		}
+		};
+	}
 }
--- src/main/java/com/example/bookstore/domain/Book.java
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -4,28 +4,35 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 
 @Entity
 public class Book {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
-	
 	private String title;
 	private String author;
 	private int year;
 	private int isbn;
 	private double price;
 	
+	@ManyToOne
+	@JoinColumn(name = "categoryid")
+	private Category category;
+	
 	public Book() {}
 
-	public Book(String title, String author, int year, int isbn, double price) {
+	public Book(String title, String author, int year, int isbn, double price, Category category) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
+		this.category = category;
 	}
 
 	public Long getId() {
@@ -67,8 +74,20 @@ public class Book {
 		this.price = price;
 	}
 
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
 	@Override
 	public String toString() {
+	if (this.category != null)
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + this.getCategory() + "]";
+	else
 		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
 				+ ", price=" + price + "]";
 	}
